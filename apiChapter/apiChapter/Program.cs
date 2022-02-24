0e6f889a9c3e3fdd6034e21f074074409ae5cbd0
RuanLucas-1232 <88802868+RuanLucas-1232@users.noreply.github.com>
2022-02-24 16:50:07 -0300

    Update Program.cs

--- apiChapter/apiChapter/Program.cs
+++ apiChapter/apiChapter/Program.cs
@@ -9,28 +9,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
-// builder.Services
-//     .AddAuthentication(options =>
-//     {
-//         options.DefaultAuthenticateScheme = "JwtBearer";
-//         options.DefaultChallengeScheme = "JwtBearer";
-//     })
-
-//     .AddJwtBearer("JwtBearer", options => 
-//     {
-//         options.TokenValidationParameters = new TokenValidationParameters
-//         {
-//             ValidateIssuer = true,
-//             ValidateAudience = true,
-//             ValidateLifetime = true,
-            
-//             IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("chapter-chave-autenticacao")),
-//             ClockSkew = TimeSpan.FromMinutes(30),
-//             ValidIssuer = "chapter.webapi",
-//             ValidAudience = "chapter.webapi"      
-//         };
-//     });
-
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy",
@@ -44,7 +22,6 @@ builder.Services.AddCors(options =>
 
 builder.Services.AddScoped<ChapterContext, ChapterContext>();
 builder.Services.AddTransient<LivroRepository, LivroRepository>();
-builder.Services.AddTransient<UsuarioRepository, UsuarioRepository>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
